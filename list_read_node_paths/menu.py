9a0ea31a5c7e4d76cda4f583830e6d3ef6f6f626
ravfx777 <ravfx777@gmail.com>
2024-12-26 15:24:57 +0900

    Delete list_read_node_paths directory

--- list_read_node_paths/menu.py
+++
@@ -1,6 +0,0 @@
-import list_read_node_paths  # スクリプトのインポート
-
-# カスタムメニューを作成
-toolbar = nuke.toolbar("Nodes")
-custom_menu = toolbar.addMenu("Custom Tools")
-custom_menu.addCommand("List Read Node Paths", "list_read_node_paths.list_read_node_paths()")
